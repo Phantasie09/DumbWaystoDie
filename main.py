de4eea1071e5102f98474e9f56b816308b43394d
Phantasie09 <mitplayer03@gmail.com>
2022-10-31 11:52:27 +0100

    with neuer file

--- main.py
+++ main.py
@@ -1,15 +1,35 @@
 import numpy as np
 from numba import *
+
 import time
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Strg+F8 to toggle the breakpoint.
+
+global v
+global A
+
+
+v = np.array([3, -1])
+A = np.array([[2, -1], [-4, 8]])
+
 
 
 # Press the green button in the gutter to run the script.
 
+def drucker():
+    print(A[0, 1])
+    print(A.shape)
+    print(A.size)
+    print(v + v)
+    print(A * v)
+    print(np.dot(v, v))
+    print(v * v)
+
+
+def main():
+    print(np.linspace (0,100,5))
+    print(np.arange(0,1,0.1))
+    print(np.eye(3))
 
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    main()
 
 
