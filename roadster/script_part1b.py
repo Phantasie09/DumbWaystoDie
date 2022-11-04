1d8417732aa3504cb832994776dce6293df6f4c2
Felix Diemling <felix.diemling@gmail.com>
2022-11-04 17:40:22 +0100

    part 1 finish

--- roadster/script_part1b.py
+++ roadster/script_part1b.py
@@ -2,20 +2,24 @@ import numpy as np
 from roadster import *
 import matplotlib.pyplot as plt
 
-
-def plot(name):
+plt
+def plot(name, n = 10000):
+    plt.figure()
     distance_km, speed_kmph = load_route(f'{name}')
-    position = np.linspace(0, max(distance_km), 100000)
-    speed=velocity(position,'speed_anna')
-    plt.plot(position, speed, '-', markersize = 1)
-    plt.scatter(distance_km, speed_kmph, s = [3]*len(speed_kmph), marker = 'd', color = 'r')
-    plt.xlabel(r'$v$ / km/h')
-    plt.ylabel(r'$m$ / km')
-    plt.title('Part 1b: Consumption data')
+    position = np.linspace(0, max(distance_km), n)
+    speed=velocity(position, name)
+    plt.plot(position, speed, '-', markersize = 0.1, label = f'interpolation n = {n}')
+    plt.scatter(distance_km, speed_kmph, s = [50]*len(speed_kmph), marker = '+', color = 'r', label = 'data')
+    plt.xlabel(r'$distance$ / km')
+    plt.ylabel(r'$speed$ / km/h')
+    plt.title(f'Part 1b: Consumption data: {name}')
     plt.savefig(fname='plot 1b')
+    plt.legend()
     plt.show()
+    #plt.close()
     #plt.cla()
 plot("speed_anna.npz")
+plot("speed_elsa.npz")
 
 
 
