4a8c839c23cc084c3f6030f79c371b68f021b271
Phantasie09 <mitplayer03@gmail.com>
2022-11-14 12:31:50 +0100

    Steckdose

---
+++ workout/CreativeMode.py
@@ -0,0 +1,19 @@
+import numpy as np
+from roadster import *
+import matplotlib.pyplot as plt
+from scipy.integrate import trapezoid
+import math
+
+def Warm(h,b=1,a=0):
+    points=np.arange(a,b+h,h)
+    return h*(sum(f(points))-(f(points[0])+f(points[-1]))/2)
+def f(x):
+    return (x ** 2) * (math.e ** x)
+
+
+def main():
+    print(Warm(1/4))
+
+
+if __name__ == '__main__':
+    main()
