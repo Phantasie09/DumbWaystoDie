3e573d47d1340df2c85efd4dc131e32bf7595aeb
Felix Diemling <felix.diemling@gmail.com>
2022-11-15 20:02:31 +0100

    add rep part 2

--- hanneskannes/script_part2.py
+++ hanneskannes/script_part2.py
@@ -3,6 +3,8 @@ from roadster import *
 import matplotlib.pyplot as plt
 from scipy.integrate import trapezoid
 
+#2a
+
 elsa='speed_elsa.npz'
 anna='speed_anna.npz'
 def time_to_destination (x , route , n ):
@@ -12,6 +14,23 @@ def time_to_destination (x , route , n ):
     t = trapezoid(speed,position)
     return h*(np.sum(speed) - (speed[0] + speed[-1])/2)
 
+# =============================================================================
+# names = [elsa, anna]
+# N = np.arange(50, 6000, 10)
+# 
+# for name in names: 
+#     time = [time_to_destination(load_route(name)[0][-1], name, n)*60 for n in N]
+#     print(name, time_to_destination(load_route(name)[0][-1], name, 2000)*60, 'min')
+#     plt.plot(N, time, label = name)
+#     
+# plt.xlabel('n / 1')
+# plt.ylabel('t / min')
+# plt.legend()
+# plt.title('2a: Arrival time')
+# 
+# =============================================================================
+
+#2b
 
 def total_consumption (x , route , n ):
     h = x/n
@@ -19,7 +38,22 @@ def total_consumption (x , route , n ):
     mjamjam  = consumption(velocity(position, route))
     t = trapezoid(mjamjam ,position)
     return h*(np.sum(mjamjam ) - (mjamjam [0] + mjamjam [-1])/2)
-  
+
+# =============================================================================
+# names = [elsa, anna]
+# N = np.arange(1, 250, 1)
+# 
+# for name in names: 
+#     time = [total_consumption(load_route(name)[0][-1], name, n)/1000 for n in N]
+#     print(name, total_consumption(load_route(name)[0][-1], name, 200)/1000, 'kWh')
+#     plt.plot(N, time, label = name)
+#     
+# plt.xlabel('n / 1')
+# plt.ylabel('E / kWh')
+# plt.legend()
+# plt.title('2b: Total electricity consumption')
+# =============================================================================
+
 
 def timekonverter(x): #WIth Inspiration of Concept INput in Sec
     if x // (365.2425*24*3600) < 1: # year
@@ -41,27 +75,29 @@ def timekonverter(x): #WIth Inspiration of Concept INput in Sec
         years=x // (365.2425*24*3600)
         return [int(years),"years",int(days),"days"]
 
-def call(name,n,x):
-    user=(name.split("_")[1]).split(".")[0]
-    print("User: "+user+", Numerations: "+str(n),", Distance:" +str(x))
-    print("time:")
-    zeit=time_to_destination(x, name, n) * 3600
-    print(*timekonverter(zeit))
-    print("Fuel:")
-    fuel=(total_consumption(x, name, n) )
-    print(fuel)
-    return zeit,fuel
-
-def main():
-    fueldata=[]
-    n=10
-    for y in [50]:
-        for i in range(1,11):
-            fueldata.append(call(anna,n*i,y)[0])
-    print(fueldata)
-
-if __name__ == '__main__':
-    main()
+# =============================================================================
+# def call(name,n,x):
+#     user=(name.split("_")[1]).split(".")[0]
+#     print("User: "+user+", Numerations: "+str(n),", Distance:" +str(x))
+#     print("time:")
+#     zeit=time_to_destination(x, name, n) * 3600
+#     print(*timekonverter(zeit))
+#     print("Fuel:")
+#     fuel=(total_consumption(x, name, n) )
+#     print(fuel)
+#     return zeit,fuel
+# 
+# def main():
+#     fueldata=[]
+#     n=10
+#     for y in [50]:
+#         for i in range(1,11):
+#             fueldata.append(call(anna,n*i,y)[0])
+#     print(fueldata)
+# 
+# if __name__ == '__main__':
+#     main()
+# =============================================================================
     
 # 2c
 
@@ -88,7 +124,7 @@ plt.legend()
 plt.title('2(c) Convergence study:')
 plt.grid(which="both")
     
-    
+ 
     
     
     
