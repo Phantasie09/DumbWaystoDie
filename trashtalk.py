0188d5964d9f8f7dcdc1653c90859c9784d84cfa
Felix Diemling <felix.diemling@gmail.com>
2022-11-24 16:45:13 +0100

    Add something beautiful

--- trashtalk.py
+++ trashtalk.py
@@ -2,68 +2,36 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
-P0 = [-10, 0.5]
-#obere line
-P1 = [-1.8, 1.5]
-P2 = [-1.6, 3]
-P3 = [-1.2, 2.8]
-P4 = [0, 2.9]
-#untere line
-p1 = [-7, 0]
-p2 = [-4, -1]
-p3 = [-1.5, -3]
-p4 = [0, -2.6]
+def f(x):
+    return np.sin(x)
 
-#obere Linie
-def f1(x):
-    X = [P0[0], -4, P1[0]]
-    Y = [P0[1], 2, P1[1]]
-    a, b, c = np.polyfit(X, Y, 2)
-    return a*x**2 + b*x + c
-
-def f2(x):
-    X = [P1[0], -1.7, P2[0]]
-    Y = [P1[1], 2, P2[1]]
-    a, b, c = np.polyfit(X, Y, 2)
-    return a*x**2 + b*x + c
-
-def f3(x):
-    X = [P2[0], P3[0]]
-    Y = [P2[1], P3[1]]
-    a, b = np.polyfit(X, Y, 1)
-    return a*x + b
-
-def f4(x):
-    X = [P3[0], 0, -P3[0]]
-    Y = [P3[1], P4[1], P3[1]]
-    a, b, c = np.polyfit(X, Y, 2)
-    return a*x**2 + b*x + c
-
-#untere Line
-
-def f5(x):
-    X = [P0[0], -8, p1[0]]
-    Y = [P0[1], 0.4, p1[1]]
-    a, b, c = np.polyfit(X, Y, 2)
-    return a*x**2 + b*x + c
-
-def f6(x):
-    X = [p1[0], -8, p2[0]]
-    Y = [p1[1], 0.4, p2[1]]
-    a, b, c = np.polyfit(X, Y, 2)
-    return a*x**2 + b*x + c
+xmin, xmax = 0, 8
+n = 2
 
 
-x1 = np.linspace(P0[0], P1[0], 100)
-x2 = np.linspace(P1[0], P2[0], 100)
-x3 = np.linspace(P2[0], P3[0], 100)
-x4 = np.linspace(P3[0], P4[0], 100)
-x5 = np.linspace(P0[0], p1[0], 100)
+X = list(np.linspace(xmin, xmax, n*2+1))
+Y = [f(x) for x in X]
 
-plt.plot(x1, f1(x1))
-plt.plot(x2, f2(x2))
-plt.plot(x3, f3(x3))
-plt.plot(x4, f4(x4))
-plt.plot(x5, f5(x5))
 
+def draw(X, Y):
+    if len(X) != len(Y):
+        print('not same size')
+        return
+    i = 0
+    while i < len(X)-1:
+        x = [X[i], X[i+1], X[i+2]]
+        y = [Y[i], Y[i+1], Y[i+2]]
+        a, b, c = np.polyfit(x, y, 2)
+        def f (x, a, b, c):
+            return a*x**2 + b*x + c
+        XX = np.linspace(X[i], X[i+2])
+        plt.plot(XX, f(XX, a, b, c))
+        i += 2
+        
+print(X)
+
+draw(X, Y)
+x = np.arange(xmin, xmax, 0.1)
+plt.plot(x, f(x), '--')
+plt.plot(X, Y, marker = 'o', ls = '')
 
